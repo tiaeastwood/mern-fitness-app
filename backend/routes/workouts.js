60f46ad59c2cf31beed0826f9e43800bcfc76ee1
Tia Eastwood <teastwood88@gmail.com>
2022-09-30 15:48:40 +0100

    add routes

---
+++ backend/routes/workouts.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+// this is to get all workouts
+router.get("/", (req, res) => {
+	res.json({ msg: "get all workouts" });
+});
+
+// get a single workout
+router.get("/:id", (req, res) => {
+	res.json({ msg: "get a single workout" });
+});
+
+// post a single workout
+router.post("/", (req, res) => {
+	res.json({ msg: "post a single workout" });
+});
+
+// delete a single workout
+router.post("/:id", (req, res) => {
+	res.json({ msg: "delete a single workout" });
+});
+
+// update (patch) a single workout
+router.patch("/:id", (req, res) => {
+	res.json({ msg: "update a single workout" });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
+const express = require("express")
+const workoutRoutes = require("./routes/workouts")
+require("dotenv").config()
 
+// express app
 const app = express();
 
-app.get("/", (req, res) => {
-	res.json({ msg: "welcome to the app" });
+// middleware (logs the request and method, then moves onto the next mehod)
+app.use((req, res, next) => {
+	console.log(req.path, req.method);
+	next();
 });
+
+app.use("/api/workouts", workoutRoutes);
+
+// app.get("/", (req, res) => {
+// 	res.json({ msg: "welcome to the app" });
+// });
 
 app.listen(process.env.PORT, () => {
 	console.log(`Now listening on port ${process.env.PORT} !!!`);
