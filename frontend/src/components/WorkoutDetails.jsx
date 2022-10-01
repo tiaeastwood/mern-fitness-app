e702a74c1e4b2607f1fb8529e443f36f74b61d0b
Tia Eastwood <teastwood88@gmail.com>
2022-10-01 17:14:04 +0100

    add delete to context

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,20 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+	const { dispatch } = useWorkoutsContext();
+	const handleClick = async () => {
+		const response = await fetch(
+			`http://localhost:4000/api/workouts/${workout._id}`,
+			{
+				method: "DELETE",
+			},
+		);
+		const json = await response.json();
+		if (response.ok) {
+			dispatch({ type: "DELETE_WORKOUT", payload: json });
+		}
+	};
+
 	return (
 		<div className="workout-details">
 			<h4>{workout.title}</h4>
@@ -11,6 +27,7 @@ const WorkoutDetails = ({ workout }) => {
 				{workout.reps}
 			</p>
 			<p>{workout.createdAt}</p>
+			<span onClick={handleClick}>Delete</span>
 		</div>
 	);
 };
